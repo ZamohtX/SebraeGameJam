e08b9a0141f385a1d6ae9227aab689ce94182d30
ZamohtX <thomazx4v13r@gmail.com>
2026-06-05 19:53:22 -0300

    Model Passageiro

---
+++ Assets/Scripts/Models/Passanger.cs
@@ -0,0 +1,27 @@
+public enum PassengerStatus
+{
+    Intact,    // Inteiro/Não roubado
+    Robbed,    // Roubado
+    Thief      // É o ladrão da parti   da
+}
+
+public class Passenger
+{
+    public string Id { get; private set; }
+    public int GridX { get; set; }
+    public int GridY { get; set; }
+    public PassengerStatus Status { get; set; }
+    public int SpriteId { get; private set; }
+    public int ClothingColorId { get; private set; }
+
+    // Construtor para inicializar o passageiro com suas características visuais e posição
+    public Passenger(string id, int x, int y, int spriteId, int colorId)
+    {
+        Id = id;
+        GridX = x;
+        GridY = y;
+        SpriteId = spriteId;
+        ClothingColorId = colorId;
+        Status = PassengerStatus.Intact; // Todo passageiro começa intacto
+    }
+}
