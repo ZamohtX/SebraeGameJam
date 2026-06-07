138bf19b0ad1420a184598adbc003ce7c43388ca
bunny-sammy <davidenacrene@gmail.com>
2026-06-07 15:05:27 -0300

    Tentativa de fix

---
+++ Assets/Scripts/Core/RouteManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RouteManager : MonoBehaviour
+{
+    public static RouteManager Instance { get; private set; }
+
+    [Header("Cenários das Paradas de Maceió")]
+    [SerializeField] private Sprite[] stationBackgrounds; 
+    
+    [Header("Referências de UI da Parada")]
+    [SerializeField] private Image stationBackgroundImageComponent; 
+    [SerializeField] private GameObject stationPanel; 
+
+    private int currentStationIndex = 0;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    public void ShowCurrentStation()
+    {
+        if (stationBackgrounds == null || stationBackgrounds.Length == 0) return;
+        if (stationBackgroundImageComponent == null || stationPanel == null) return;
+
+        int index = currentStationIndex % stationBackgrounds.Length;
+        stationBackgroundImageComponent.sprite = stationBackgrounds[index];
+        stationPanel.SetActive(true);
+    }
+
+    public void AdvanceToNextStation()
+    {
+        currentStationIndex++;
+    }
+
+    public void HideStation()
+    {
+        if (stationPanel != null) stationPanel.SetActive(false);
+    }
+}
